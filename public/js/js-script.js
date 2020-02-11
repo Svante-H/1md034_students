9295500633ad3ab2e94fc00efcbbf6799f5f5047
Svante Hornfeldt <svante.hornfeldt@gmail.com>
2020-02-11 14:32:14 +0100

    adding vue and js objects to html

--- public/js/js-script.js
+++ public/js/js-script.js
@@ -1,17 +1,50 @@
-function menuItem(name, calories, containsGluten, containsLactose, imgSrc) {
-    this.name = name;
-    this.calories = calories;
-    this.containsGluten = containsGluten;
-    this.containsLactose = containsLactose;
-    this.imgSrc = imgSrc; //Is this the property described?
+class menuItem {
+
+    constructor(name, calories, containsGluten, containsLactose, imgSrc) {
+        this.name = name;
+        this.calories = calories;
+        this.containsGluten = containsGluten;
+        this.containsLactose = containsLactose;
+        this.imgSrc = imgSrc; //Is this the property described?
+
+    }
+    getName(){
+        return this.name;
+    }
+
+    getCalories(){
+        return this.calories;
+    }
 }
 
-function burgerInfo(burger){
-    return burger.name + ' ' + burger.calories;
-}
 let burger1 = new menuItem("Big Mac", 775, true, true);
 let burger2 = new menuItem("Cheeseburger", 662, false, true);
 let burger3 = new menuItem("Vegan Mushroom", 512, false, false);
 let burger4 = new menuItem("Chicken burger", 443, true, true);
 let burger5 = new menuItem("Halloumi burger", 731, false, true);
-burgerInfo(burger1, burger2);
+
+/* putting js objects into html element, may be a more elegant way*/
+let b1Node = document.createElement("p");
+let b1Text = document.createTextNode(burger1.getName() + ': ' + burger1.getCalories());
+b1Node.appendChild(b1Text);
+document.getElementById("selectBurger").appendChild(b1Node);
+
+let b2Node = document.createElement("p");
+let b2Text = document.createTextNode(burger2.getName() + ': ' + burger2.getCalories());
+b2Node.appendChild(b2Text);
+document.getElementById("selectBurger").appendChild(b2Node);
+
+let b3Node = document.createElement("p");
+let b3Text = document.createTextNode(burger3.getName() + ': ' + burger3.getCalories());
+b3Node.appendChild(b3Text);
+document.getElementById("selectBurger").appendChild(b3Node);
+
+let b4Node = document.createElement("p");
+let b4Text = document.createTextNode(burger4.getName() + ': ' + burger4.getCalories());
+b4Node.appendChild(b4Text);
+document.getElementById("selectBurger").appendChild(b4Node);
+
+let b5Node = document.createElement("p");
+let b5Text = document.createTextNode(burger5.getName() + ': ' + burger5.getCalories());
+b5Node.appendChild(b5Text);
+document.getElementById("selectBurger").appendChild(b5Node);
--- public/js/vue-script.js
+++ public/js/vue-script.js
@@ -1,7 +1,10 @@
-const vm = new Vue({
-    el: '#myID',
-    data: {
-        x: 'Välj en burgare'
-    }
 
-})
+const vm = new Vue({
+    el: '#selectBurger',
+    data: {
+        bNo1: burger1.getName() + ': ' + burger1.getCalories(),
+        bNo2: burger2.getName() + ': ' + burger2.getCalories(),
+        bNo3: burger3.getName() + ': ' + burger3.getCalories(),
+        bNo4: burger4.getName() + ': ' + burger4.getCalories(),
+        bNo5: burger5.getName() + ': ' + burger5.getCalories()
+    })
