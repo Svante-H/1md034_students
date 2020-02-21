816da73b9d3d2feb55ee8cde798792c6a9b2fc4e
Svante Hornfeldt <svante.hornfeldt@gmail.com>
2020-02-21 13:20:47 +0100

    Finished last part of lab

--- public/js/vue-script.js
+++ public/js/vue-script.js
@@ -29,7 +29,16 @@ const socket = io();
 const vmWhole = new Vue({
     el: "#document",
     data: {
-        orders: {},
+        localOrder: {
+            orderId : 0,
+            position: {
+                x: 0,
+                y: 0,
+                },
+            display: "T",
+
+        },
+        isMapClicked: false,
         food: food,
         inputData: {
             name: "",
@@ -38,27 +47,10 @@ const vmWhole = new Vue({
             gender: "",
         }
     },
-        created: function() {
-            /* When the page is loaded, get the current orders stored on the server.
-             * (the server's code is in app.js) */
-            socket.on('initialize', function(data) {
-                this.orders = data.orders;
-            }.bind(this));
-
-            /* Whenever an addOrder is emitted by a client (every open map.html is
-             * a client), the server responds with a currentQueue message (this is
-             * defined in app.js). The message's data payload is the entire updated
-             * order object. Here we define what the client should do with it.
-             * Spoiler: We replace the current local order object with the new one. */
-            socket.on('currentQueue', function(data) {
-                this.orders = data.orders;
-            }.bind(this));
-    },
     methods: {
         getInput: function () {
             let insertParent = document.getElementById("insertText");
             insertParent.innerHTML = ""; //Resets the printed text field if the button is pressed multiple times (probably not necessary in real situation)
-
             inputData.name = document.getElementById("fullname").value;
             inputData.email = document.getElementById("mail").value;
             inputData.payment = document.getElementById("payment").value;
@@ -70,55 +62,57 @@ const vmWhole = new Vue({
                 }
             }
             let insertStr = orderToString(inputData);
-            insertStr += "\nOrders made: ";
+            insertStr += "\nOrders made: (";
 
+            let burgerList = [];
             for (let burger of vmWhole.food) {
+
                 let box = document.getElementById(burger.name);
                 if (box.checked) {
-                    insertStr += burger.name + ", "
+                    burgerList.push(burger.name);
+                    insertStr += burger.name + ", ";
                 }
             }
-
             if (!anyCheckbox()) {
                 console.log("No burger has been chosen");
             }
+            else{
+                let lastCommaIndex = insertStr.lastIndexOf(',');
+                insertStr = insertStr.substring(0,lastCommaIndex) + ')';
+            }
 
-            console.log(orderToString(inputData));
+
+
             insertParent.append(document.createTextNode(insertStr));
+            vmWhole.addOrder(burgerList);
+        },
 
-        },
-        getNext: function() {
-            /* This function returns the next available key (order number) in
-             * the orders object, it works under the assumptions that all keys
-             * are integers. */
-            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-                return Math.max(last, next);
-            }, 0);
-            return lastOrder + 1;
-        },
-        addOrder: function(event) {
-            /* When you click in the map, a click event object is sent as parameter
-             * to the function designated in v-on:click (i.e. this one).
-             * The click event object contains among other things different
-             * coordinates that we need when calculating where in the map the click
-             * actually happened. */
+        displayOrder: (event) =>{
             let offset = {
                 x: event.currentTarget.getBoundingClientRect().left,
                 y: event.currentTarget.getBoundingClientRect().top,
             };
+            vmWhole.isMapClicked = true;
+            vmWhole.localOrder.position.x = event.clientX - 10 - offset.x;
+            vmWhole.localOrder.position.y = event.clientY - 10 - offset.y;
+        },
+
+        getNext: function() {
+            vmWhole.localOrder.orderId++;
+            return vmWhole.localOrder.orderId;
+        },
+        addOrder: function(list) {
             socket.emit('addOrder', {
-                orderId: this.getNext(),
-                details: {
-                    x: event.clientX - 10 - offset.x,
-                    y: event.clientY - 10 - offset.y,
-                },
-                orderItems: ['Beans', 'Curry'],
-            })
+                orderId: vmWhole.getNext(),
+                details: this.localOrder,
+                orderItems: list,
+                customerInfo : {name : inputData.name, gender : inputData.gender, email: inputData.email, payment : inputData.payment}
+
+            });
         },
     }
 
 });
-
 //Should probably split the getInput function for readability
 
 
